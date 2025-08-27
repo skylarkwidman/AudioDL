ad69f9665a14282835ee781afb88914c34e6f9f0
Skylar Widman <skylarkwidman@gmail.com>
2025-08-26 20:40:29 -0400

    added coreaudioml files

---
+++ test_cuda.py
@@ -0,0 +1,14 @@
+import torch
+
+print(torch.__version__)
+print(torch.version.cuda)
+print(torch.cuda.is_available())
+print(torch.cuda.device_count())
+print(torch.cuda.get_device_name(0) if torch.cuda.is_available() else "CUDA not available")
+
+
+
+
+
+
+
